565cb543b93daf97baba6e9b2448ae0463944848
daretechie <deeprince2020@gmail.com>
2025-06-22 10:48:56 +0100

    Updtaed

--- kenya_optimized.py
+++ kenya_optimized.py
@@ -414,9 +414,9 @@ def main():
                 logging_dir=f"./logs/fold_{fold+1}",
                 logging_steps=CONFIG["logging_steps"],
                 save_steps=CONFIG["save_steps"],
-                eval_steps=CONFIG["eval_steps"],
+                eval_steps="steps",  # Use steps for evaluation
                 # evaluation_strategy="steps", #commented out to avoid frequent evals
-                # save_strategy="steps",
+                save_strategy="steps",
                 save_total_limit=CONFIG["save_total_limit"],
                 load_best_model_at_end=CONFIG["load_best_model_at_end"],
                 metric_for_best_model=CONFIG["metric_for_best_model"],
